9899e19c42c93b094695a273d555aea09514f33e
SnehaY20 <21snehay@gmail.com>
2025-06-20 01:24:07 +0530

    feat: adding login route

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -7,7 +7,7 @@ const ErrorResponse = require("../utils/errorResponse.js");
  * @route     POST /api/v1/auth/register
  * @access    Public
  */
-const registerUser = asyncHandler(async (req, res, next) => {
+exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
 
   if (!name || !email || !password) {
@@ -31,14 +31,42 @@ const registerUser = asyncHandler(async (req, res, next) => {
   res.status(201).json({
     success: true,
     token,
-    user: {
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
-      createdAt: user.createdAt,
-    },
   });
 });
 
-module.exports = { registerUser };
+/**
+ * @desc      Login user
+ * @route     POST /api/v1/auth/login
+ * @access    Public
+ */
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email & password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
+
+
