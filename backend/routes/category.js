54281a1632fda6801a501d2f5bbbe4fb841f3a51
Sneha Yadav <105566600+SnehaY20@users.noreply.github.com>
2025-06-22 05:16:24 +0530

    feat: Added delete category api (#9)
    * feat: adding delete category api
    
    * removed unnecessary console logs

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -4,11 +4,13 @@ const {
   getCategories,
   createCategory,
   updateCategory,
+  deleteCategory
 } = require("../controllers/category.js");
 const auth = require("../middleware/auth");
 
 router.get("/category", auth, getCategories);
 router.post("/category", auth, createCategory);
 router.put("/category/:id", auth, updateCategory);
+router.delete("/category/:id", auth, deleteCategory);
 
 module.exports = router;
