ea5e7993a84430db8f920069638cfce5971964c1
Sneha Yadav <105566600+SnehaY20@users.noreply.github.com>
2025-06-22 04:56:01 +0530

    refactor: Added update category API

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,3 +1,4 @@
+const logger = require("../config/logger");
 const ErrorResponse = require("../utils/errorResponse");
 
 const errorHandler = (err, req, res, next) => {
@@ -5,6 +6,8 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   const statusCode = err.statusCode || 500;
+
+  logger.error(error);
 
   res.status(statusCode).json({
     success: false,
--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getCategories, createCategory } = require("../controllers/category.js");
+const {
+  getCategories,
+  createCategory,
+  updateCategory,
+} = require("../controllers/category.js");
 const auth = require("../middleware/auth");
-
 
 router.get("/category", auth, getCategories);
 router.post("/category", auth, createCategory);
+router.put("/category/:id", auth, updateCategory);
 
 module.exports = router;
