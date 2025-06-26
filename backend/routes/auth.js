b2ac79c462f049a96bdf7d5351d33d622a37b456
SnehaY20 <21snehay@gmail.com>
2025-06-26 19:04:18 +0530

    feat: adding profile and update password controller methods

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/auth.js");
+const {
+  register,
+  login,
+  profile,
+  updatePassword,
+} = require("../controllers/auth.js");
+const auth = require("../middleware/auth");
 
 router.post("/register", register);
-router.post("/login", login)
+router.post("/login", login);
+router.get("/profile", auth, profile);
+router.put("/:id", auth, updatePassword);
 
 module.exports = router;
